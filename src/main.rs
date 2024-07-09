fc8bd34e5e393d847b3387c979f0458e2866ac87
sakuram-dev <trge241@gmail.com>
2024-07-09 16:02:38 +0900

    #2/copy data from STDIN

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,28 @@
 use arboard::Clipboard;
 use clap::Parser;
+use std::io::{self, Read};
 
 #[derive(Parser, Debug)]
 #[command(about = "Copy data from STDIN to the clipboard.")]
 #[command(version, long_about = None)]
 struct Args {
-    string: Option<String>,
-
     #[arg(short, long, default_value_t = 1)]
     count: u8,
 }
 
 fn main() {
-    let mut clipboard = Clipboard::new().unwrap();
-    
-    let the_string = clipboard.get_text().unwrap_or_else(|_| {
-        println!("Clipboard is empty");
-        String::new()
-    });
-
     let args = Args::parse();
-    let the_string = &args.string.unwrap_or(the_string);
+    let mut clipboard = Clipboard::new().unwrap();
+    let mut input_data = String::new();
 
-    for _ in 0..args.count  {
-        println!("Hello, {}!", the_string);
-        clipboard.set_text(the_string.to_owned()).unwrap();
+    io::stdin()
+        .read_to_string(&mut input_data)
+        .expect("Failed to read from STDIN");
+
+    for _ in 0..args.count {
+        println!("Copy to clipboard \n{}", input_data);
+        clipboard
+            .set_text(input_data.clone())
+            .expect("Failed to copy to clipboard");
     }
 }
