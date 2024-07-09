58e6b34a0709b5605975b7bc3364290a1528b679
sakuram-dev <trge241@gmail.com>
2024-07-09 16:52:13 +0900

    #6/copy from file

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,36 @@ use std::io::{self, Read};
 #[derive(Parser, Debug)]
 #[command(about = "Copy data from STDIN to the clipboard.")]
 #[command(version, long_about = None)]
-struct Args {}
+struct Args {
+    #[clap(short, long)]
+    #[clap(help = "copy from file instead of STDIN")]
+    file: Option<String>,
+}
 
 fn main() {
-    let _args = Args::parse();
+    let args = Args::parse();
     let mut clipboard = Clipboard::new().unwrap();
     let mut input_data = String::new();
 
-    io::stdin()
-        .read_to_string(&mut input_data)
-        .expect("Failed to read from STDIN");
+    if let Some(file) = args.file {
+        let mut file_data = String::new();
+        std::fs::File::open(file)
+            .expect("Failed to open file")
+            .read_to_string(&mut file_data)
+            .expect("Failed to read file");
 
-    println!("Copy to clipboard \n{}", input_data);
-    clipboard
-        .set_text(input_data.clone())
-        .expect("Failed to copy to clipboard");
+        println!("Copy to clipboard \n{}", file_data);
+        clipboard
+            .set_text(file_data.clone())
+            .expect("Failed to copy to clipboard");
+    } else {
+        io::stdin()
+            .read_to_string(&mut input_data)
+            .expect("Failed to read from STDIN");
+
+        println!("Copy to clipboard \n{}", input_data);
+        clipboard
+            .set_text(input_data.clone())
+            .expect("Failed to copy to clipboard");
+    }
 }
