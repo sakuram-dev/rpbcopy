c6d1398951ab99308d13aa97dc4001bf27df0d90
sakuram-dev <trge241@gmail.com>
2024-07-09 16:17:55 +0900

    #4/remove count

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,10 @@ use std::io::{self, Read};
 #[derive(Parser, Debug)]
 #[command(about = "Copy data from STDIN to the clipboard.")]
 #[command(version, long_about = None)]
-struct Args {
-    #[arg(short, long, default_value_t = 1)]
-    count: u8,
-}
+struct Args {}
 
 fn main() {
-    let args = Args::parse();
+    let _args = Args::parse();
     let mut clipboard = Clipboard::new().unwrap();
     let mut input_data = String::new();
 
@@ -19,10 +16,8 @@ fn main() {
         .read_to_string(&mut input_data)
         .expect("Failed to read from STDIN");
 
-    for _ in 0..args.count {
-        println!("Copy to clipboard \n{}", input_data);
-        clipboard
-            .set_text(input_data.clone())
-            .expect("Failed to copy to clipboard");
-    }
+    println!("Copy to clipboard \n{}", input_data);
+    clipboard
+        .set_text(input_data.clone())
+        .expect("Failed to copy to clipboard");
 }
